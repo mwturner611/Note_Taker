b862bb4be9e9f5750a4a8ed44b01e14841d2779e
mwturner611 <mwturner611@gmail.com>
2020-04-16 19:47:50 -0500

    packages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -12,6 +12,10 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
